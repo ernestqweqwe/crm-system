788c557bd8d28ac13f465a0e1e44022141cec872
Ernestas Yushkiavichus <ernest.bel@mail.ru>
2025-03-04 08:54:55 +0300

    feat: sidebar design

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,13 +1,48 @@
-import { Layout } from 'antd'
+import { Button, Layout, Menu } from 'antd'
 import { Link } from 'react-router'
 import './SideBar.scss'
+import { useState } from 'react'
+import {
+    ArrowLeftOutlined,
+    ArrowRightOutlined,
+    UnorderedListOutlined,
+    UserOutlined,
+} from '@ant-design/icons'
 
 export const SideBar = () => {
+    const [collapsed, setCollapsed] = useState(false)
     return (
-        <Layout.Sider width={300} className="side-bar">
-            <Link to="/">Task List</Link>
-            <Link to="/profile">Profile</Link>
+        <Layout.Sider
+            trigger={null}
+            theme="light"
+            width={248}
+            collapsible
+            collapsed={collapsed}
+            className="side-bar"
+            onCollapse={(value) => setCollapsed(value)}
+        >
+            <h1 className="logo">Todo App</h1>
+            <Menu
+                theme="light"
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                items={[
+                    {
+                        key: '1',
+                        icon: <UserOutlined />,
+                        label: <Link to="/profile">Profile</Link>,
+                    },
+                    {
+                        key: '2',
+                        icon: <UnorderedListOutlined />,
+                        label: <Link to="/">Task List</Link>,
+                    },
+                ]}
+            />
+
+            <Button className="trigger-btn" onClick={() => setCollapsed((prev) => !prev)}>
+                {collapsed ? <ArrowRightOutlined /> : <ArrowLeftOutlined />}
+            </Button>
         </Layout.Sider>
-        // Todo использывать компонент Layout.Slider
     )
 }
--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,13 +1,14 @@
-import { Button, Form, Input, message, Space } from 'antd'
+import { Button, Form, Input, Layout, message, Row, Space } from 'antd'
 import './RegistrationPage.scss'
 import authService from 'api/services/AuthService'
 import { AxiosError } from 'axios'
 import { NoticeType } from 'antd/es/message/interface'
-import { Link } from 'react-router'
+import { Link as RouterLink } from 'react-router'
 
 export const RegistrationPage = () => {
     const [form] = Form.useForm()
     const [messageApi, contextHolder] = message.useMessage()
+    const { Item } = Form
 
     // TODO разобраться с валидацией номера телефона
     // TODO Когда использовать try catch а когда than
@@ -36,143 +37,162 @@ export const RegistrationPage = () => {
         }
     }
     return (
-        <div className="registration-page">
-            <h1 className="title">Registration</h1>
-            <Form
-                layout={'vertical'}
-                form={form}
-                style={{ width: 600 }}
-                onFinish={() => handleSubmit()}
-            >
-                <Form.Item
-                    name={'username'}
-                    label={'User name'}
-                    rules={[
-                        { required: true, message: 'User name field is required' },
-                        { min: 1, message: 'Min 2 symbols' },
-                        { max: 64, message: 'Max 64 symbols' },
-                        {
-                            pattern: /[а-яА-Яa-zA-Z]/,
-                            message: 'Symbols must be of the russian or latin alphabet',
-                        },
-                    ]}
-                    hasFeedback
-                >
-                    <Input size="large" placeholder="User name"></Input>
-                </Form.Item>
-                <Form.Item
-                    name="login"
-                    label="Login"
-                    rules={[
-                        { required: true, message: 'Login field is required' },
-                        { min: 2, message: 'Min 2 symbols' },
-                        { max: 64, message: 'Max 64 symbols' },
+        <Layout style={{ minHeight: '100vh' }}>
+            <Row style={{ height: '100vh', flexWrap: 'nowrap', padding: 10, overflow: 'hidden' }}>
+                <div className="img-container">
+                    <img src="/assets/login_page_img.png" alt="skeleton" />
+                </div>
+                <div className="form-container__registration">
+                    <div className="form-content">
+                        <div className="description">
+                            <h1>Кegister a new account</h1>
+                            <p>See what is going on with your business</p>
+                        </div>
 
-                        {
-                            pattern: /[a-zA-Z]+$/,
-                            message: 'Symbols must be of the latin alphabet',
-                        },
-                    ]}
-                    hasFeedback
-                >
-                    <Input size="large" placeholder="Login"></Input>
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    label="Password"
-                    rules={[
-                        { required: true, message: 'Input field is required' },
-                        {
-                            min: 6,
-                            message: 'Min 6 symbols',
-                        },
-                        {
-                            max: 60,
-                            message: 'Max 60 symbols',
-                        },
-                    ]}
-                    hasFeedback
-                >
-                    <Input.Password size="large" placeholder="Password"></Input.Password>
-                </Form.Item>
-                <Form.Item
-                    dependencies={['password']}
-                    hasFeedback
-                    name={confirm}
-                    label="Confirm Password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please confirm your password!',
-                        },
-                        ({ getFieldValue }) => ({
-                            validator(_, value) {
-                                if (!value || getFieldValue('password') === value) {
-                                    return Promise.resolve()
-                                }
-                                return Promise.reject(
-                                    new Error('The new password that you entered do not match!')
-                                )
-                            },
-                        }),
-                    ]}
-                >
-                    <Input.Password size="large" placeholder="Repeat password"></Input.Password>
-                </Form.Item>
-                <Form.Item
-                    name="email"
-                    label="Email"
-                    rules={[
-                        { required: true, message: 'Email is required field' },
-                        {
-                            type: 'email',
-                            message: 'Incorrect email',
-                        },
-                    ]}
-                    hasFeedback
-                >
-                    <Input size="large" placeholder="Email"></Input>
-                </Form.Item>
-                <Form.Item
-                    name="phoneNumber"
-                    label="Phone"
-                    required={false}
-                    rules={[
-                        {
-                            whitespace: true,
-                        },
-                        {
-                            validator: (_, value) => {
-                                if (!/^\+[1-9]/.test(value)) {
-                                    return Promise.reject(
-                                        'Incorrect phone format, must be +123123123'
-                                    )
-                                }
-                                if (value.length < 8) {
-                                    return Promise.reject('Min 8 symbols')
-                                }
-                                if (value.length > 15) {
-                                    return Promise.reject('Max 15 symbols')
-                                }
-                                return Promise.resolve()
-                            },
-                        },
-                    ]}
-                    hasFeedback
-                >
-                    <Input size="large" placeholder="Telephone number"></Input>
-                </Form.Item>
-                <Space style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                    <Link style={{ padding: 15 }} to="/login">
-                        Sign in
-                    </Link>
-                </Space>
-                {contextHolder}
-            </Form>
-        </div>
+                        <img src="/assets/login_page_icon.png" alt="icon" />
+                        <Form layout={'vertical'} form={form} onFinish={() => handleSubmit()}>
+                            <Item
+                                name={'username'}
+                                label={'User name'}
+                                rules={[
+                                    { required: true, message: 'User name field is required' },
+                                    { min: 1, message: 'Min 2 symbols' },
+                                    { max: 64, message: 'Max 64 symbols' },
+                                    {
+                                        pattern: /[а-яА-Яa-zA-Z]/,
+                                        message: 'Symbols must be of the russian or latin alphabet',
+                                    },
+                                ]}
+                                hasFeedback
+                            >
+                                <Input size="large" placeholder="User name"></Input>
+                            </Item>
+                            <Item
+                                name="login"
+                                label="Login"
+                                rules={[
+                                    { required: true, message: 'Login field is required' },
+                                    { min: 2, message: 'Min 2 symbols' },
+                                    { max: 64, message: 'Max 64 symbols' },
+
+                                    {
+                                        pattern: /[a-zA-Z]+$/,
+                                        message: 'Symbols must be of the latin alphabet',
+                                    },
+                                ]}
+                                hasFeedback
+                            >
+                                <Input size="large" placeholder="Login"></Input>
+                            </Item>
+                            <Item
+                                name="password"
+                                label="Password"
+                                rules={[
+                                    { required: true, message: 'Input field is required' },
+                                    {
+                                        min: 6,
+                                        message: 'Min 6 symbols',
+                                    },
+                                    {
+                                        max: 60,
+                                        message: 'Max 60 symbols',
+                                    },
+                                ]}
+                                hasFeedback
+                            >
+                                <Input.Password
+                                    size="large"
+                                    placeholder="Password"
+                                ></Input.Password>
+                            </Item>
+                            <Item
+                                dependencies={['password']}
+                                hasFeedback
+                                name={confirm}
+                                label="Confirm Password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please confirm your password!',
+                                    },
+                                    ({ getFieldValue }) => ({
+                                        validator(_, value) {
+                                            if (!value || getFieldValue('password') === value) {
+                                                return Promise.resolve()
+                                            }
+                                            return Promise.reject(
+                                                new Error(
+                                                    'The new password that you entered do not match!'
+                                                )
+                                            )
+                                        },
+                                    }),
+                                ]}
+                            >
+                                <Input.Password
+                                    size="large"
+                                    placeholder="Repeat password"
+                                ></Input.Password>
+                            </Item>
+                            <Item
+                                name="email"
+                                label="Email"
+                                rules={[
+                                    { required: true, message: 'Email is required field' },
+                                    {
+                                        type: 'email',
+                                        message: 'Incorrect email',
+                                    },
+                                ]}
+                                hasFeedback
+                            >
+                                <Input size="large" placeholder="Email"></Input>
+                            </Item>
+                            <Item
+                                name="phoneNumber"
+                                label="Phone"
+                                required={false}
+                                rules={[
+                                    {
+                                        whitespace: true,
+                                    },
+                                    {
+                                        validator: (_, value) => {
+                                            if (!/^\+[1-9]/.test(value)) {
+                                                return Promise.reject(
+                                                    'Incorrect phone format, must be +123123123'
+                                                )
+                                            }
+                                            if (value.length < 8) {
+                                                return Promise.reject('Min 8 symbols')
+                                            }
+                                            if (value.length > 15) {
+                                                return Promise.reject('Max 15 symbols')
+                                            }
+                                            return Promise.resolve()
+                                        },
+                                    },
+                                ]}
+                                hasFeedback
+                            >
+                                <Input size="large" placeholder="Telephone number"></Input>
+                            </Item>
+                            <Button className="registration-btn" htmlType="submit">
+                                Registration
+                            </Button>
+                            {contextHolder}
+                        </Form>
+                    </div>
+                    <Space className="register">
+                        <span>Have account?</span>
+                        <RouterLink to="/login" className="link">
+                            Login
+                        </RouterLink>
+                    </Space>
+                </div>
+            </Row>
+            {contextHolder}
+        </Layout>
     )
 }
 //Todo submit активен только когда все поля заполнены
